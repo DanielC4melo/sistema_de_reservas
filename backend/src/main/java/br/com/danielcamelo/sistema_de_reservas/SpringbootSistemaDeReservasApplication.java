3292bfcc38a3dc7f052dee60046eed454acbfa23
CameloDaniel <daniel.camelo@edu.unirio.br>
2025-11-14 23:08:25 -0300

    Configura o Spring Boot e a conexão com o MySQL

---
+++ backend/src/main/java/br/com/danielcamelo/sistema_de_reservas/SpringbootSistemaDeReservasApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielcamelo.sistema_de_reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSistemaDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSistemaDeReservasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/danielcamelo/sistema_de_reservas/SpringbootSistemaDeReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielcamelo.sistema_de_reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSistemaDeReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
